ebaf3cbe45723df96dc8853baa7ffe1d2d7e5168
Niels Grove-Rasmussen <niels@grove-rasmussen.net>
2018-04-30 15:23:58 +0200

    Powershell commands added

--- Spikes/Active Directory/DomainController.ps1
+++ Spikes/Active Directory/DomainController.ps1
@@ -2,7 +2,7 @@
 .DESCRIPTION
   Create Active Directory (AD) Domain Controller (DC) with new domain in new forrest.
 
-  Windows Server 2016 Core (1709)
+  Windows Server 2016 - later Windows Server 2016 Core (1709) or Windows Server 2019 Core
 .LINK
   How to build a server 2016 domain controller (Non-GUI) and make it secure
   (https://medium.com/@rootsecdev/how-to-build-a-server-2016-domain-controller-non-gui-and-make-it-secure-4e784b393bac)
@@ -12,13 +12,28 @@
 #>
 
 #region Server
+# Change adapter to vmxnet3
+
+
 ###  Configure server with "sconfig"  ###
 
 # 1) Create computer name (option 2)
+#    (https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-5.1)
+Rename-Computer -NewName 'DC00' -Restart
 
 # 2) Configure static IP address (option 8)
+#    (https://docs.microsoft.com/en-us/powershell/module/nettcpip/set-netipaddress?view=win10-ps)
+Set-NetIPAddress -InterfaceIndex ... -IPAddress 192.168.42.42
+# Win32_NetworkAdapterConfiguration class  (https://msdn.microsoft.com/en-us/library/aa394217.aspx)  (https://blogs.technet.microsoft.com/heyscriptingguy/2012/02/28/use-powershell-to-configure-static-ip-and-dns-settings/)
+Win32_NetworkAdapterConfiguration
 
 # 3) Set date and time (option 9)
+
+# 4) Windows Location: UK - UTC
+
+# 5) Time format: UK ISO 8601'ish
+
+# 6) Windows SmartScreen: Enabled
 
 # Exit to command line (option 15)
 #endregion Server
